9b2f7afea3ff5f8276bb9992a648738173617cf3
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-06-06 16:20:28 +0530

    Started with day6 css
    initial commit

---
+++ 002-numbers/index.js
@@ -0,0 +1,105 @@
+// //Numbers
+// Write a program to input 2 numbers and display the sum of the numbers to the console.
+const Number1 = 20;
+const Number2= 40
+Sum = Number1 + Number2
+console.log(Sum);
+
+
+// Write a JavaScript program to calculate the simple interest given P,R,T with the given formula. Formula: SI = (P * T * R) / 100 Where: P = principal amount T = time R = rate SI = simple interest
+ var p,t,r, SI;
+ p=100;
+ r=6;
+ t=2;
+ SI = (p *t*r)/100
+ console.log(SI);
+
+
+// Input: P=100, R=6%, T=2
+// Output: 12
+// Write a program to calculate the kinetic energy of a body with mass 'm' and volume 'v'.
+
+// Formula : 0.5 * m * v * v
+
+const V= 10;
+const m = 100;
+KE = (0.5 *m * V * V)
+console.log(KE);
+
+// Write a program to convert Fahrenheit to Celsius. For Fahrenheit to Celsius conversion use: C = (F - 32) * 5/9 'F' and 'C' are the temperatures on the Fahrenheit scale and Celsius scale respectively.
+
+// Input: 56
+// Output: 13.33333
+
+
+// Calculate the area, perimeter of a square of side 'a'. Also, calculate the surface area and the volume of a cube of side 'a'.
+
+// Formula :
+const A = 10;
+Area = (A * A);
+// Perimeter(4 * A);
+// SA = (6 *A *A);
+// Volume (A * A * A);
+console.log(Area);
+// console.log(Perimeter)
+// console.log(SA);
+// console.log(Volume);
+
+// Area: a*a
+
+// Perimeter: 4*a
+
+// Surface Area: 6*a*a
+
+// Volume: a*a*a
+
+// Given the Cost Price(CP) and Selling Price(SP) of a product. Write a Program to Calculate the Profit or Loss.
+
+// Input: CP = 1500, SP = 2000
+// Output: 500 Profit
+const CP = 1500, SP = 2000;
+profit = SP - CP
+console.log("profit is : ",profit)
+
+// Input: CP = 3125, SP = 1125
+// Output: 2000 Loss
+const cp1 = 3125, sp1 = 1125;
+loss = cp1-sp1;
+console.log("loss is : " ,loss);
+// Write a program to calculate sum of N natural digits, as input by the users?
+
+// Enter a positive integer: 100
+// Sum = 5050
+// Write a Program to Print N Odd Number in Descending Order.
+
+// Input : 4
+// Output : 7
+// 5
+// 3
+// 1
+// Write a JavaScript program to compute the sum of all digits that occur in a given string.
+
+
+// Input: 1234
+// Output: 1+2+3+4 = 10
+// Write a JavaScript program that reverses a number.
+
+// Example:
+
+// Input:  32243;
+// Output:  34223
+// Write a Program to cyclically Rotate a Number by X positions in the left direction, as provided by the user.
+
+// Example-
+
+// Enter a Number : 1234
+// Enter the Number of Rotations : 2
+// Output : 3412
+// Write a Program to convert Decimal to Binary.
+
+// Enter the number to convert: 5
+// Binary of Given Number is=101
+// Follow up Question : Write a Program to Convert Octal to Decimal.
+
+// Enter an octal number: 116
+// Octal of Given Number = 78 in decimal
